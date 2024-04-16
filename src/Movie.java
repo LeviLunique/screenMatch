b1e7bb5120241e1311c62a5531389a3a5e28f757
LeviLunique <levi.lunique@gmail.com>
2024-04-15 20:11:44 -0400

    Add Movie class with essential attributes

---
+++ src/Movie.java
@@ -0,0 +1,8 @@
+public class Movie {
+    private String title;
+    private int releaseYear;
+    private boolean includedInSubscription;
+    private double rating;
+    private int ratingCount;
+    private int durationMinutes;
+}
