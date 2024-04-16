70a09bdf55686ea4c225072fbd4d32f6dc6785b4
LeviLunique <levi.lunique@gmail.com>
2024-04-16 18:24:10 -0400

    Movie methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Movie myMovie = new Movie();
+        myMovie.title = "The Godfather";
+        myMovie.releaseYear = 1970;
+        myMovie.durationMinutes = 180;
+
+        myMovie.showInfo();
+        myMovie.rankMovie(8);
+        myMovie.rankMovie(5);
+        myMovie.rankMovie(10);
+
+        System.out.println(myMovie.rating);
+        System.out.println(myMovie.ratingCount);
+        System.out.println(myMovie.getAverage());
     }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,8 +1,22 @@
 public class Movie {
-    private String title;
-    private int releaseYear;
-    private boolean includedInSubscription;
-    private double rating;
-    private int ratingCount;
-    private int durationMinutes;
+    String title;
+    int releaseYear;
+    boolean includedInSubscription;
+    double rating;
+    int ratingCount;
+    int durationMinutes;
+
+    void showInfo() {
+        System.out.println("Movie title: " + this.title);
+        System.out.println("Release year: " + this.releaseYear);
+    }
+
+    void rankMovie(double score){
+        rating += score;
+        ratingCount++;
+    }
+
+    double getAverage () {
+        return rating / ratingCount;
+    }
 }
