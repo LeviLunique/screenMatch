b074d78b6f0896c2970cd7d21fbda75997a538ae
LeviLunique <levi.lunique@gmail.com>
2024-04-17 19:21:54 -0400

    Movie Encapsulation

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         Movie myMovie = new Movie();
-        myMovie.title = "The Godfather";
-        myMovie.releaseYear = 1970;
-        myMovie.durationMinutes = 180;
+        myMovie.setTitle("The Godfather");
+        myMovie.setReleaseYear(1970);
+        myMovie.setDurationMinutes(180);
 
         myMovie.showInfo();
         myMovie.rankMovie(8);
         myMovie.rankMovie(5);
         myMovie.rankMovie(10);
 
-        System.out.println(myMovie.rating);
-        System.out.println(myMovie.ratingCount);
+        System.out.println(myMovie.getRating());
+        System.out.println(myMovie.getRatingCount());
         System.out.println(myMovie.getAverage());
     }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,22 +1,62 @@
 public class Movie {
-    String title;
-    int releaseYear;
-    boolean includedInSubscription;
-    double rating;
-    int ratingCount;
-    int durationMinutes;
+    private String title;
+    private int releaseYear;
+    private boolean isIncludedInSubscription;
+    private double rating;
+    private int ratingCount;
+    private int durationMinutes;
 
-    void showInfo() {
+    public String getTitle() {
+        return this.title;
+    }
+
+    public int getReleaseYear() {
+        return this.releaseYear;
+    }
+
+    public boolean isIncludedInSubscription() {
+        return this.isIncludedInSubscription;
+    }
+
+    public int getDurationMinutes() {
+        return this.durationMinutes;
+    }
+    public int getRatingCount() {
+        return this.ratingCount;
+    }
+
+    public double getRating () {
+        return this.rating;
+    }
+
+    public double getAverage () {
+        return this.getRating() / this.getRatingCount();
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public void setDurationMinutes(int durationMinutes) {
+        this.durationMinutes = durationMinutes;
+    }
+
+    public void setIncludedInSubscription(boolean isIncludedInSubscription) {
+        this.isIncludedInSubscription = isIncludedInSubscription;
+    }
+
+
+    public void showInfo() {
         System.out.println("Movie title: " + this.title);
         System.out.println("Release year: " + this.releaseYear);
     }
 
-    void rankMovie(double score){
-        rating += score;
-        ratingCount++;
-    }
-
-    double getAverage () {
-        return rating / ratingCount;
+    public void rankMovie(double score){
+        this.rating += score;
+        this.ratingCount++;
     }
 }
